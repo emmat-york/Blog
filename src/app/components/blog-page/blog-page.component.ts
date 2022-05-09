22c07fcb5da73a02677998aa2d4367b24a8645d9
emmat-york <andrei.filimonchik@gmail.com>
2022-05-10 00:34:05 +0400

    Blog: Update BlogPage fonts

--- src/app/components/blog-page/blog-page.component.ts
+++ src/app/components/blog-page/blog-page.component.ts
@@ -20,5 +20,9 @@ export class BlogPageComponent implements OnInit {
   // Todo: Make directive for changing input background color and color
   public changeInputState(eventState: boolean): void {
     this.isInputFocused = eventState;
+
+    if(eventState) {
+      window.scrollTo({ top: 420, behavior: 'smooth' });
+    }
   }
 }
